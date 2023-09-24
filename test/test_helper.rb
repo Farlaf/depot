b2853e506697b692bc6f4bea108cf9dda401ee0e
Oleg Slepukhov <slepuhovoleg@gmail.com>
2023-09-24 20:21:10 +0400

    disable parallelize

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,7 +22,7 @@ require 'rails/test_help'
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    ENV['RAILS_ENV'] || parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
