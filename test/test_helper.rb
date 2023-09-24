03cc8443408cc43484a947ad508c5cb43ba2944d
Oleg Slepukhov <slepuhovoleg@gmail.com>
2023-09-24 20:34:11 +0400

    set settings from old project

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,21 @@
 require 'simplecov'
 
-SimpleCov.start 'rails' do
-  # if ENV['CI']
-    require 'simplecov-lcov'
+if ENV['CI']
+  SimpleCov.start('rails') do
+    if ENV['COVERAGE']
+      require 'simplecov-lcov'
 
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
+      SimpleCov::Formatter::LcovFormatter.config do |c|
+        c.report_with_single_file = true
+        c.single_report_path = 'coverage/lcov.info'
+      end
+
+      formatter SimpleCov::Formatter::LcovFormatter
+
     end
 
-    formatter SimpleCov::Formatter::LcovFormatter
-  # end
-
-  add_filter %w[version.rb initializer.rb]
+    add_filter ['version.rb', 'initializer.rb', 'config.rb']
+  end
 end
 
 ENV['RAILS_ENV'] ||= 'test'
