2445140fbd9eed45e28e28eb6363aa0a1d68b7d2
Oleg Slepukhov <slepuhovoleg@gmail.com>
2023-12-25 13:12:08 +0400

    added switcher

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,11 +5,15 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
 
-    session[:counter] = 0 if session[:counter].nil?
-    session[:counter] += 1
+      session[:counter] = 0 if session[:counter].nil?
+      session[:counter] += 1
 
-    @user_visit = session[:counter]
+      @user_visit = session[:counter]
+    end
   end
 end
